3348ade9e03008dd8162cd529018f71c9eb0ff8a
Matt Engerer <64572411+uchutanjyo@users.noreply.github.com>
2022-09-04 15:25:28 -0400

    Time: 121 ms (13.29%), Space: 44.2 MB (13.47%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function (operations) {
+    let x = 0;
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i] === "--X") {
+            x--;
+        } else if (operations[i] === "X--") {
+            x--;
+        } else if (operations[i] === "++X") {
+            x++;
+        } else if (operations[i] === "X++") {
+            x++;
+        } else {
+            return undefined;
+        }
+    }
+    console.log(x)
+    return x;
+};
